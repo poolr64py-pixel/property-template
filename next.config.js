ebb6f077fbce492e3aee99adc6651b04f32ff812
poolr64py-pixel <poolr64py@gmail.com>
2025-09-03 11:50:27 -0300

    Corrigir next.config.js, remover conflitos

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -15,22 +13,4 @@ const nextConfig = {
   },
 };
 
-=======
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  
-  // Configuração de internacionalização
-  i18n: {
-    // Idiomas suportados
-    locales: ['pt', 'es', 'en', 'de'],
-    // Idioma padrão
-    defaultLocale: 'pt',
-    // Detecção automática de idioma baseada no header Accept-Language
-    localeDetection: false,
-  },
-};
-
->>>>>>> 228e52862bd1487979d8c411c9e94465e04f3bce
 module.exports = nextConfig;
