43cc3024f6880068686d4013a544539b52d3a160
poolr64py-pixel <poolr64py@gmail.com>
2025-09-03 11:58:32 -0300

    Resolver conflitos e preparar build

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
-=======
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
->>>>>>> 228e52862bd1487979d8c411c9e94465e04f3bce
